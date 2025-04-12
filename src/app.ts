2eeea91d6f13790994f8e7a7c1170d618f9b5a23
ddas4548 <ddas4548@gmail.com>
2025-04-12 15:48:50 +0530

    Express TypeScript starter project

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+
+const port =3000
+
+app.get('/ping',(req,res)=>{
+    res.send("pong")
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on http://localhost:${port}`)
+})
