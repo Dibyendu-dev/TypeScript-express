9f703e7de81498ade3ed0875f149def10547cf1d
ddas4548 <ddas4548@gmail.com>
2025-04-17 16:08:21 +0530

    added error handler

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { serverConfig } from "./config/server.config";
 
 import v1Router from "./routers/v1/v1.router";
 import v2Router from "./routers/v2/v2.routes";
+import { genericErrorHandler } from "./middleware/error.middleware";
 
 const app = express();
 app.use(express.json())
@@ -10,6 +11,10 @@ app.use(express.json())
 
 app.use("/api/v1", v1Router);
 app.use("/api/v2", v2Router);
+
+
+// add the error handler middleware
+app.use(genericErrorHandler)
 
 console.log("Enviroment variable are loaded");
 
--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,12 +1,23 @@
-import { Request, Response } from "express";
-export const pingHandler = (req:Request, res:Response) : void => {
-    console.log("request body is",req.body)
-    console.log("query params is",req.query)
-    console.log("url params is", req.params)
+import { NextFunction, Request, Response } from "express";
+import fs from "fs/promises";
+import { InternalServerError } from "../utils/errors/app.error";
+export const pingHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  // console.log("request body is",req.body)
+  // console.log("query params is",req.query)
+  // console.log("url params is", req.params)
 
-    res.send("pong");
+  try {
+    await fs.readFile("sample");
+    res.status(200).json({ message: "pong" });
+  } catch (error) {
+    throw new InternalServerError("something went wrong")
   }
+};
 
-  export const healthHandler = (req:Request, res:Response) : void => {
-    res.status(200).send("All OK");
-  }
+export const healthHandler = (req: Request, res: Response): void => {
+  res.status(200).send("All OK");
+};
